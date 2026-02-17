1adc1dff40959540077327e3be1765af90d84e17
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-02-17 16:49:46 -0300

    feat: adiciona alarme

--- sketch.ino
+++ sketch.ino
@@ -15,7 +15,11 @@ const int trigInt = 19, echoInt = 21;
 const int pinoServo = 13;
 const int distDeteccao = 50;
 const int pinoBotao = 4; 
+const int pinoBuzzer = 25;
+
 int estado = 0; 
+unsigned long tempoAbertura = 0; 
+unsigned long travaSensoresAte = 0; 
 
 long lerDistancia(int trig, int echo) {
   digitalWrite(trig, LOW); delayMicroseconds(2);
@@ -26,52 +30,50 @@ long lerDistancia(int trig, int echo) {
   return (d <= 0) ? 400 : d;
 }
 
-void setup_wifi() {
-  delay(10);
-  Serial.print("Conectando WiFi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
-  }
-  Serial.println(" Conectado!");
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Tentando MQTT...");
-    if (client.connect("ESP32_Cancela_Zapella")) { 
-      Serial.println(" Conectado ao Broker!");
-      client.subscribe("cancela/comando");
-    } else {
-      delay(5000);
-    }
-  }
+void abrirCancela(String origem) {
+  Serial.println("Abrindo: " + origem);
+  cancela.write(90);
+  
+  String mensagem = "Aberta por: " + origem;
+  client.publish("cancela/status", mensagem.c_str());
+  
+  delay(3000); 
+  cancela.write(0);
+  client.publish("cancela/status", "Livre");
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  String msg;
+  String msg = "";
   for (int i = 0; i < length; i++) { msg += (char)payload[i]; }
-
   if (msg == "ABRIR") {
-    Serial.println("Comando Remoto: Abrindo Cancela");
-    cancela.write(90);
-    client.publish("cancela/status", "Aberta Manualmente");
-    delay(3000); 
-    cancela.write(0);
-    client.publish("cancela/status", "Livre");
+    abrirCancela("Comando Remoto");
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  
   pinMode(pinoBotao, INPUT_PULLUP);
+  pinMode(pinoBuzzer, OUTPUT);
   cancela.attach(pinoServo);
   cancela.write(0);
+  
   pinMode(trigExt, OUTPUT); pinMode(echoExt, INPUT);
   pinMode(trigInt, OUTPUT); pinMode(echoInt, INPUT);
+  Serial.println("\nSistema Pronto!");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ESP32_Cancela_Zapella")) {
+      client.subscribe("cancela/comando");
+    } else { delay(5000); }
+  }
 }
 
 void loop() {
@@ -79,47 +81,60 @@ void loop() {
   client.loop();
 
   if (digitalRead(pinoBotao) == LOW) { 
-    Serial.println("Botão Físico: Abrindo...");
-    client.publish("cancela/status", "Abertura Manual (Wokwi)");
-    cancela.write(90);
-    delay(3000); 
-    cancela.write(0);
-    client.publish("cancela/status", "Livre");
+    abrirCancela("Botao Fisico");
   }
 
-  long dExt = lerDistancia(trigExt, echoExt);
-  long dInt = lerDistancia(trigInt, echoInt);
+  if (millis() > travaSensoresAte) {
+    long dExt = lerDistancia(trigExt, echoExt);
+    long dInt = lerDistancia(trigInt, echoInt);
 
-  if (estado == 0) {
-    if (dExt < distDeteccao) {
-      Serial.println("Entrada Detectada");
-      client.publish("cancela/status", "Carro Entrando"); 
-      cancela.write(90);
-      estado = 1; 
-    } 
-    else if (dInt < distDeteccao) {
-      Serial.println("Saída Detectada");
-      client.publish("cancela/status", "Carro Saindo");
-      cancela.write(90);
-      estado = 2; 
+    if (estado == 0) {
+      if (dExt < distDeteccao) {
+        client.publish("cancela/status", "Carro Entrando"); 
+        cancela.write(90);
+        tempoAbertura = millis();
+        estado = 1; 
+      } 
+      else if (dInt < distDeteccao) {
+        client.publish("cancela/status", "Carro Saindo");
+        cancela.write(90);
+        tempoAbertura = millis();
+        estado = 2; 
+      }
     }
-  }
-  else if (estado == 1) {
-    if (dInt < distDeteccao) {
-      while(lerDistancia(trigInt, echoInt) < distDeteccao) { delay(100); }
-      client.publish("cancela/status", "Livre - Entrada Concluída");
-      delay(1000); 
-      cancela.write(0);
-      estado = 0;
+    else if (estado == 1) { 
+      if (dInt < distDeteccao) {
+        while(lerDistancia(trigInt, echoInt) < distDeteccao) { delay(100); }
+        cancela.write(0);
+        estado = 0;
+        client.publish("cancela/status", "Livre");
+      }
+      else if (millis() - tempoAbertura > 15000) { 
+        tone(pinoBuzzer, 1000);
+        client.publish("cancela/status", "ALARME: Travado por 10s");
+        delay(5000); 
+        noTone(pinoBuzzer);
+        cancela.write(0);
+        travaSensoresAte = millis() + 10000; 
+        estado = 0;
+      }
     }
-  }
-  else if (estado == 2) {
-    if (dExt < distDeteccao) {
-      while(lerDistancia(trigExt, echoExt) < distDeteccao) { delay(100); }
-      client.publish("cancela/status", "Livre - Saída Concluída");
-      delay(1000);
-      cancela.write(0);
-      estado = 0;
+    else if (estado == 2) {
+      if (dExt < distDeteccao) {
+        while(lerDistancia(trigExt, echoExt) < distDeteccao) { delay(100); }
+        cancela.write(0);
+        estado = 0;
+        client.publish("cancela/status", "Livre");
+      }
+      else if (millis() - tempoAbertura > 15000) { 
+        tone(pinoBuzzer, 1000);
+        client.publish("cancela/status", "ALARME: Travado por 10s");
+        delay(5000);
+        noTone(pinoBuzzer);
+        cancela.write(0);
+        travaSensoresAte = millis() + 10000; 
+        estado = 0;
+      }
     }
   }
   delay(50);
